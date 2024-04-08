79fccefafa6417efe2878dc7ee53405f174b452e
chanw12 <wooju9812127@gmail.com>
2024-04-08 13:57:10 +0900

    init

---
+++ src/main/java/com/ll/whev/WhevApplication.java
@@ -0,0 +1,13 @@
+package com.ll.whev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhevApplication.class, args);
+    }
+
+}
